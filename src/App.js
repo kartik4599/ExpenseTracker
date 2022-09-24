acd0d6af691408a78e26859837806dbba370c9cb
devloper-kartik <kartikmendu2@gmail.com>
2022-09-24 14:30:15 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -27,7 +27,6 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
       <Expenses items={expenses} />
     </div>
   );
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,28 +5,19 @@ import Card from '../UI/Card';
 import './Expenses.css';
 
 const Expenses = (props) => {
+    const list=[]
+    props.items.forEach(element => {
+        list.push(
+            <ExpenseItem
+        title={element.title}
+        amount={element.amount}
+        date={element.date}
+      />
+        )
+    });
   return (
     <Card className="expenses">
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
+      {list}
     </Card>
   );
 }
