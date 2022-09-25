558374d0b0135a14567cc091516b6754dd841db7
devloper-kartik <kartikmendu2@gmail.com>
2022-09-25 13:37:57 +0530

    Expense List

---
+++ src/components/Expenses/ExpenseList.js
@@ -0,0 +1,38 @@
+import "./ExpenseList.css";
+import ExpenseItem from "./ExpenseItem"; 
+const ExpenseList = (props)=>{
+    //If There are no items
+    if(props.items.length<1){
+        return <h2 className="expenses-list__fallback ">Found No Item</h2>
+    }
+    //If there is only one item
+    if(props.items.length===1){
+      return props.items.map((element)=>{
+          return (
+            <ul className="expenses-list">
+            <ExpenseItem
+                key={element.id}
+                title={element.title}
+                amount={element.amount}
+                date={element.date}
+            />
+            <h3 className="expenses-list__fallback ">Only single Expense here. Please add more... </h3>
+            </ul>);
+       }); 
+    }
+    //it There are more than one item
+    return props.items.map((element)=>{
+      return( 
+      <ul className="expenses-list">
+        <ExpenseItem
+            key={element.id}
+            title={element.title}
+            amount={element.amount}
+            date={element.date}
+        />
+      </ul>);
+    }); 
+  
+}
+
+export default ExpenseList;
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
-
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
 import ExpenseFilter from './ExpenseFilter';
+import ExpenseList from './ExpenseList';
 
 const Expenses = (props) => {
   const [Year,changeYear]=useState('2019');
@@ -16,41 +15,11 @@ const Expenses = (props) => {
     return element.date.getFullYear().toString()===Year;
   }));
 
-  let ExpenseContent = <p>There are No Expenses In {Year}</p>;
-
-  if(FilterExpenses.length===1){
-    ExpenseContent = FilterExpenses.map((element)=>{
-      return (
-      <div>
-        <ExpenseItem
-            key={element.id}
-            title={element.title}
-            amount={element.amount}
-            date={element.date}
-          />
-        <p>Only single Expense here. Please add more... </p>
-      </div>)
-          ;
-          
-       }); 
-  }
-
-  if(FilterExpenses.length>1){
-    ExpenseContent = FilterExpenses.map((element)=>{
-      return <ExpenseItem
-            key={element.id}
-            title={element.title}
-            amount={element.amount}
-            date={element.date}
-          />
-       }); 
-  }
-
 
   return (
     <Card className="expenses">
       <ExpenseFilter selected={Year} onChangeFilter={filterChangeHandler}/>
-      {ExpenseContent}
+      <ExpenseList items={FilterExpenses}/>
     </Card>
   );
 }
